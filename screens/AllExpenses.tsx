f258beb9415eab3939855091ab68b4cf603420d1
Jose Espejo <joseespejo@yahoo.com>
2024-04-30 17:32:19 +0100

    feat: add ExpensesContext to AllExpenses

--- screens/AllExpenses.tsx
+++ screens/AllExpenses.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
-import ExpensesOutput from "../components/ExpensesOutput";
+import ExpensesOutput from "../components/expenses-output/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
 
 const AllExpenses = () => {
-  return <ExpensesOutput expenses={[]} expensesPeriod="Total" />;
+  const { expenses } = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput
+      expenses={expenses}
+      expensesPeriod="Total"
+      fallbackText="No expenses found"
+    />
+  );
 };
 
 export default AllExpenses;
