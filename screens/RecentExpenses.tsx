1c0e47d2db493dfbb752817bf728f4c40f223721
Jose Espejo <joseespejo@yahoo.com>
2024-04-30 17:34:41 +0100

    feat: display expenses for the last 7 days

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
-import ExpensesOutput from "../components/ExpensesOutput";
+import ExpensesOutput from "../components/expenses-output/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
 
 const RecentExpenses = () => {
-  return <ExpensesOutput expenses={[]} expensesPeriod="Recent" />;
+  const { expenses } = useContext(ExpensesContext);
+
+  const recentExpenses = expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 days"
+      fallbackText="No expenses registered for the last 7 days"
+    />
+  );
 };
 
 export default RecentExpenses;
